8713a38e3b89359ab00fa0d4ab6732804c53a75c
Narthorn <narthorn@gmail.com>
2016-02-11 12:37:03 +0100

    Update to use new API functions

--- Fix.lua
+++ Fix.lua
@@ -5,12 +5,8 @@
 --- Fix is free software, all files licensed under the GPLv3. See LICENSE for details.
 
 -- 31/01/2016 fix missing GetAccountRealmCharacter function
-if not GameLib.GetAccountRealmCharacter then
-	local char = GameLib.GetPlayerUnit()
-	if not char then Apollo.RegisterEventHandler("CharacterCreated", "f", {f = RequestReloadUI}) end
-	function GameLib.GetAccountRealmCharacter()
-		return { strCharacter = char and char:GetName() or "Temp Name", strRealm = "Redmoon", strAccount = "never" }
-	end
+function GameLib.GetAccountRealmCharacter()
+	return {strCharacter = GameLib.GetPlayerCharacterName(), strRealm = GameLib.GetRealmName(), strAccount = ""}
 end
 
 Apollo.RegisterAddon({})
